7d1294ef321a45ab63cb767bc8412397402f059b
Gabriel Moreira <145815911+gabrielmoreira-7@users.noreply.github.com>
2024-10-21 09:48:29 -0300

    Add files via upload

---
+++ src/Fundamentos/EstruturaDecisao.java
@@ -0,0 +1,17 @@
+public class EstruturaDecisao
+{
+	public static void main(String[] args)
+	{
+		int idade = 17;
+		
+		if (idade >= 18)
+		{
+			System.out.println("Maior de idade");
+		}
+
+		else
+		{
+			System.out.println("Menor de idade");
+		}
+	}
+}
---
+++ src/Fundamentos/VariaveisConstantes.java
@@ -0,0 +1,13 @@
+public class VariaveisConstantes
+{
+	public static void main(String[] args)
+	{
+		final String nome = "Gabriel"; //definindo uma constante
+		int idade = 20;
+		double altura = 1.75;
+
+		System.out.println(nome);
+		System.out.println(idade);
+		System.out.println(altura);
+	}
+}
